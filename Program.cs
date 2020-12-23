50e82e78725a8b46d8179edfc104b54ddf334b32
ajaykumarchepuri1 <ajaykumarchepuri1@gmail.com>
2020-12-23 09:33:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LDAPAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auth auth = new Auth();
+            auth.ValidateUser("ajay","ajay@123");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
